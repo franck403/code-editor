7f0ac144095a6ff00fd6dbde4ecca41b26f7e77f
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-15 15:37:15 -0400

    change thing place

--- config/main.js
+++ config/main.js
@@ -1,10 +1,11 @@
-monaco.editor.onDidCreateEditor(function (event) {
-	console.log('editor crated');
-	document.getElementById("load").remove()
-});
+
 
 require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
 require(['vs/editor/editor.main'], function () {
+	var onend = monaco.editor.onDidCreateEditor(function (event) {
+		console.log('editor crated');
+		document.getElementById("load").remove()
+	});
 	var editor = monaco.editor.create(document.getElementById('container'), {
 		value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
 		language: 'javascript'
