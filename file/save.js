f2f2387c143b6e3e1746c55056a51e91013e750c
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-16 16:55:19 -0400

    fix bug

--- file/save.js
+++ file/save.js
@@ -1,5 +1,5 @@
 function save() {
-    var elm = document.getElementsByClassName("monaco-mouse-cursor-text")
+    var elm = document.querySelectorAll('.monaco-mouse-cursor-text')
     elm.forEach((note) => {
         if (note.type = "textarea") {
             var val = note.value
