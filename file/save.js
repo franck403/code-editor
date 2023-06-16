4cec68c448b05a28d0093efaf0b54bde0150d6be
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-16 17:01:22 -0400

    addd new feature to save

--- file/save.js
+++ file/save.js
@@ -1,12 +1,17 @@
 function save() {
+    if (window.savelist == undefined) {
+        window.savelist = []
+    }
     var elm = document.querySelectorAll('.monaco-mouse-cursor-text')
     elm.forEach((note) => {
         if (note.type == "textarea") {
             var val = note.value
             var random = String(Math.random()).split(".")[1]
             var code = `window.saved${random} = "${val}"`
+            window.savelist.push(random)
             console.log(code)
             eval(code)
         }
     });
+    return window.savelist
 }
