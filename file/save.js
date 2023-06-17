d9dc877b212849042ee8aaaf3b318bb5776eab5d
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-17 10:22:20 -0400

    addd load id

--- file/save.js
+++ file/save.js
@@ -7,11 +7,33 @@ function save() {
         if (note.type == "textarea") {
             var val = note.value
             var random = String(Math.random()).split(".")[1]
-            var code = `window.saved${random} = "${val}"`
             window.savelist.push(random)
+            var myblob = new Blob([val], {
+                type: 'text/plain'
+            });
+            var code = `window.saved${random} = "${myblob}"`
             console.log(code)
             eval(code)
         }
     });
     return window.savelist
+}
+
+
+function load(id) {
+    var file = `return window.saved${id}"`
+    var file = eval(file)
+    const reader = new FileReader();
+
+    reader.addEventListener(
+    "load",
+    () => {
+        console.log(reader.result)
+    },
+    false
+    );
+
+    if (file) {
+    reader.readAsText(file);
+    }
 }
