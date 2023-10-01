3e0b486f3ccd9c8901509cc8cc276267abb4a7ef
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-01 09:04:30 -0400

    fixed most of the bug

--- file/save.js
+++ file/save.js
@@ -2,18 +2,13 @@ function save() {
     if (window.savelist == undefined) {
         window.savelist = []
     }
-    var elm = document.querySelectorAll('.monaco-mouse-cursor-text')
-    elm.forEach((note) => {
-        if (note.type == "textarea") {
-            var val = note.value
-            var random = String(Math.random()).split(".")[1]
-            window.savelist.push(random)
-            var val = val.replaceAll('"',"gcode.custom1")
-            var code = `window.saved${random} = "${val}"`
-            console.log(code)
-            eval(code)
-        }
-    });
+    var val = monaco.editor.getEditors()[0].getValue()
+    var random = String(Math.random()).split(".")[1]
+    window.savelist.push(random)
+    var val = val.replaceAll('"',"gcode.custom1")
+    var code = `window.saved${random} = "${val}"`
+    console.log(code)
+    eval(code)
     return window.savelist[-1]
 }
 
@@ -29,7 +24,7 @@ function load(id) {
     var elm = document.querySelectorAll('.monaco-mouse-cursor-text')
     elm.forEach((note) => {
         if (note.type == "textarea") {
-            note.value = runFromString(`return window.saved${id};`).replaceAll('gcode.custom1','"')
+            monaco.editor.getEditors()[0].setValue(runFromString(`return window.saved${id};`).replaceAll('gcode.custom1','"'))
         }
     });
 }
