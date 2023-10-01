954f72928df045d80c93b43f6aaabdeb74e742b6
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-01 12:04:32 -0400

    fixed bug with the last one load

--- file/save.js
+++ file/save.js
@@ -5,7 +5,7 @@ function save() {
     var random = String(Math.random()).split(".")[1]
     window.savelist.push(random)
     var val = monaco.editor.getEditors()[0].getValue().replaceAll('"',"gcode.custom1")
-    eval(`window.saved${random} = "${val}";window.saved = "${val}";`)
+    eval(`window.saved${random} = "${val}";window.saved = "${random}";`)
     return window.savelist[-1]
 }
 
