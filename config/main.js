fa2f9ab0e32c83b863f4f105ac28f536187b432b
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-02 06:54:37 -0400

    bvn

--- config/main.js
+++ config/main.js
@@ -1,5 +1,3 @@
-
-
 require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
 require(['vs/editor/editor.main'], function () {
 	var onend = monaco.editor.onDidCreateEditor(function (event) {
