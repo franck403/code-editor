bd10c9b885832d2e6bc33723f3de4e648439d4d7
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-02 06:41:40 -0400

    added new things

--- config/main.js
+++ config/main.js
@@ -3,8 +3,7 @@
 require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
 require(['vs/editor/editor.main'], function () {
 	var onend = monaco.editor.onDidCreateEditor(function (event) {
-		console.log('Gcod is ready to use');
-		document.getElementById("load").remove()
+		console.log('Gcod is loaded');
 	});
 	var editor = monaco.editor.create(document.getElementById('container'), {
 		value: "",
@@ -19,6 +18,8 @@ require(['vs/editor/editor.main'], function () {
 				insertText: keyword[1]
 			}));
 	
+			console.log('Gcod is ready to use');
+			document.getElementById("load").remove()
 			return { suggestions };
 		}
 	});	
