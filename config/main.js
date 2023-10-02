b1f5b6366c9a3775f74ba3991b011780ad54ec89
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-02 06:54:18 -0400

    FUCK

--- config/main.js
+++ config/main.js
@@ -3,8 +3,9 @@
 require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
 require(['vs/editor/editor.main'], function () {
 	var onend = monaco.editor.onDidCreateEditor(function (event) {
-		console.log('Gcod is loaded');
-	});
+		console.log('Gcod is ready to use');
+		document.getElementById("load").remove()
+});
 	var editor = monaco.editor.create(document.getElementById('container'), {
 		value: "",
 		language: 'javascript'
@@ -17,8 +18,6 @@ require(['vs/editor/editor.main'], function () {
 				kind: monaco.languages.CompletionItemKind.Keyword,
 				insertText: keyword[1]
 			}));
-			console.log('Gcod is ready to use');
-			document.getElementById("load").remove()
 			return { suggestions };
 		}
 	});	
