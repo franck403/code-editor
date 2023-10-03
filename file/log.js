22a86cb711321b975ec804e41c1300161df61ebc
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-03 11:10:17 -0400

    fixed push function

--- file/log.js
+++ file/log.js
@@ -2,7 +2,7 @@ function log(message) {
     if (window.runLog == undefined){
         window.runLog = []
     }
-    window.runLog.push[message]
+    window.runLog.push(message)
 }
 
 function clear() {
