c3cd50c2fc856832da12690c72fdce7ccf35c5f5
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-04 09:16:47 -0400

    added new way to import thing

--- config/custom.js
+++ config/custom.js
@@ -3,3 +3,10 @@ function script(file) {
     elem.src = file
     document.getElementsByTagName("head")[0].appendChild(elem)
 }
+
+function importScript(file) {
+    var elem = document.createElement("script");
+    elem.src = file
+    elem.type = "module"
+    document.getElementsByTagName("head")[0].appendChild(elem)
+}
