8040461e3fe1c755e03d8701a4abd0a6447175d8
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-04 10:55:12 -0400

    ops my fault

--- config/custom.js
+++ config/custom.js
@@ -10,3 +10,9 @@ function importScript(file) {
     elem.type = "module"
     document.getElementsByTagName("head")[0].appendChild(elem)
 }
+function importScripts(file) {
+    var elem = document.createElement("script");
+    elem.src = file
+    elem.type = "module"
+    document.getElementsByTagName("head")[0].appendChild(elem)
+}
