961d5b833fcee3b44f104b2adb7ea5cd39e7faed
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-05 13:51:37 -0400

    moved a line and fixed run

--- package/run.js
+++ package/run.js
@@ -1,6 +1,22 @@
 function run() {
+    if (window.rgtnhjgfthuhbtfrnjgtrnjgtfrjufhun) {
+        window.rgtnhjgfthuhbtfrnjgtrnjgtfrjufhun = true
+        const firebaseConfig = {
+            apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+            authDomain: "ic-hat.firebaseapp.com",
+            databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+            projectId: "ic-hat",
+            storageBucket: "ic-hat.appspot.com",
+            messagingSenderId: "720687529085",
+            appId: "1:720687529085:web:2d964e880c5e2398058514",
+            measurementId: "G-YC8K0D7GLR"
+        };
+        firebase.initializeApp(firebaseConfig);
+        var database = firebase.database();
+    }
     clearLog()
     save()
     runFromString(compile(loadLastReturn()))
     loadLog()
 }
+window.rgtnhjgfthuhbtfrnjgtrnjgtfrjufhun = false
