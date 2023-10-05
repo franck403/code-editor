e7aa521602845ecd741eed9ce7dd3cdb34f31a90
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-05 11:04:15 -0400

    add new functinalty

--- connector/session/connect.js
+++ connector/session/connect.js
@@ -1,9 +1,47 @@
-script("https://beta.chat.geoloup.com/d/session/main.js")
+function email() {
+    getEmail()
+    return localStorage.getItem("email")
+}
+
+function getEmail() {
+    sendMessage("email")
+    window.addEventListener('message', function(event) {
+        console.log("Message received from the child: " + event.data); // Message received from child
+        this.localStorage.setItem("email",event.data)
+        removeEventListener('message',window)
+    });    
+}
+
+function name() {
+    getName()
+    return localStorage.getItem("name")
+}
+
+function getName() {
+    sendMessage("name")
+    window.addEventListener('message', function(event) {
+        console.log("Message received from the child: " + event.data); // Message received from child
+        this.localStorage.setItem("name",event.data)
+        removeEventListener('message',window)
+    });    
+}
+
+
+function room() {
+    getName()
+    return localStorage.getItem("room")
+}
+
+function getRoom() {
+    sendMessage("room")
+    window.addEventListener('message', function(event) {
+        console.log("Message received from the child: " + event.data); // Message received from child
+        this.localStorage.setItem("room",event.data)
+        removeEventListener('message',window)
+    });    
+}
 
 function sendMessage(message) {
     const iframe = document.querySelector("iframe");
     iframe.contentWindow.postMessage(message, "*");
-  }
-window.addEventListener('message', function(event) {
-    console.log("Message received from the child: " + event.data); // Message received from child
-});
+}
