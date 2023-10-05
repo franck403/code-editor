f734b014beb33f6d29b28a7630ce760fbee5318e
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-05 10:48:16 -0400

    added api to allow ic-hat session to talk to ic-extention to talk

--- connector/session/connect.js
+++ connector/session/connect.js
@@ -1,1 +1,10 @@
 script("https://beta.chat.geoloup.com/d/session/main.js")
+
+function sendMessage() {
+    const message = document.querySelector("#message").value;
+    const iframe = document.querySelector("iframe");
+    iframe.contentWindow.postMessage(message, "*");
+  }
+window.addEventListener('message', function(event) {
+    console.log("Message received from the child: " + event.data); // Message received from child
+});
