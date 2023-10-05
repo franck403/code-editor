e3d65291f4465f68b79e4dc0839edd8ba6c53966
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-05 13:25:24 -0400

    test

--- file/main.js
+++ file/main.js
@@ -7,3 +7,16 @@ script("compiler/main.js")
 script("connector/session/connect.js")
 importScripts("connector/firebase.js")
 script("https://www.gstatic.com/firebasejs/8.10.0/firebase.js")
+const firebaseConfig = {
+    apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+    authDomain: "ic-hat.firebaseapp.com",
+    databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+    projectId: "ic-hat",
+    storageBucket: "ic-hat.appspot.com",
+    messagingSenderId: "720687529085",
+    appId: "1:720687529085:web:2d964e880c5e2398058514",
+    measurementId: "G-YC8K0D7GLR"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.getDatabase(app);
