166b855a874edf850771f0d8d019fb9f367f27e5
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-07 07:30:16 -0400

    adding publish function

---
+++ connector/session/publish.js
@@ -0,0 +1,21 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function  PublishExtention() {
+    getEmail()
+    getName()
+    var ext = firebase.database().ref('extention/lib/' + cusid + '/').push()
+    localStorage.ext = ext
+    ext.set({
+        email: email(),
+        user_name: Name(),
+        name: myExtName,
+        description:desc,
+        content = compileContent
+    });
+}
