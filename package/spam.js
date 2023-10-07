ff9cda4d656a1fc5bc1f16ea6a340c88ce81cc2c
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-07 07:48:22 -0400

    prevent spam

---
+++ package/spam.js
@@ -0,0 +1,10 @@
+window.gv8gvmgvrvhuhmooooooooooorrrvemhervth4rgfm84nvrht83rmhtf84h3487tfh3578mudrxttyyugjtfxdrgfvyudfxxxcgttttttttttttvjucurrent = 0
+
+gl.OnSendMessage((message) => {
+    var g = window.gv8gvmgvrvhuhmooooooooooorrrvemhervth4rgfm84nvrht83rmhtf84h3487tfh3578mudrxttyyugjtfxdrgfvyudfxxxcgttttttttttttvjucurrent
+    if (g > 10) {
+        gl.StopSendMessage()
+    }
+    window.gv8gvmgvrvhuhmooooooooooorrrvemhervth4rgfm84nvrht83rmhtf84h3487tfh3578mudrxttyyugjtfxdrgfvyudfxxxcgttttttttttttvjucurrent++
+    setTimeout(() => {window.gv8gvmgvrvhuhmooooooooooorrrvemhervth4rgfm84nvrht83rmhtf84h3487tfh3578mudrxttyyugjtfxdrgfvyudfxxxcgttttttttttttvjucurrent--},2000)
+})
