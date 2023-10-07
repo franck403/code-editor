b8c7fb3a4b3f4a74cbd00c950c65fe7e9c71c305
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-07 10:55:32 -0400

    added thing to the compiler

--- compiler/main.js
+++ compiler/main.js
@@ -1,3 +1,3 @@
 function compile(code) {
-    return code.replaceAll("Thread","async").replaceAll("global ","window.")
+    return code.replaceAll("Thread","async").replaceAll("global ","window.").replaceAll("gcod.custom3","gcod.customs3").replaceAll("gcod.custom2","gcod.customs2")
 }
